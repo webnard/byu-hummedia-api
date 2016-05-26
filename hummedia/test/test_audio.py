d20c71dede7f407dcb9e33e3588c8438842bd58d
Ian Hunter <beanland2@gmail.com>
2016-05-26 14:42:38 -0400

    Failing test for retrieiving audio file with missing collection.

--- hummedia/test/test_audio.py
+++ hummedia/test/test_audio.py
@@ -9,7 +9,7 @@ def upload_single_file_and_assert(app_instance, filename):
         'audio[]': [(f, 'audio.mp3')]
     }
     response = app_instance.post('/batch/audio/ingest', data=data)
-  
+
   data = json.loads(response.data)
   item = data[0]
 
@@ -17,7 +17,7 @@ def upload_single_file_and_assert(app_instance, filename):
 
   file = open(config.MEDIA_DIRECTORY + new_filename, 'r')
   orig = open(filename, 'r')
-  
+
   assert item['ma:title'] == 'Coin dropped on wooden floor'
   assert item['ma:date'] == 2007
   assert item['pid'] is not None
@@ -33,7 +33,7 @@ def test_upload_mp3_without_date_id3(ASSETS, ACCOUNTS, app):
         'audio[]': [(f, 'audio.mp3')]
     }
     response = app.post('/batch/audio/ingest', data=data)
-  
+
   assert response.status_code == 200
 
 def test_upload_mp3_without_any_id3(ASSETS, ACCOUNTS, app):
@@ -57,7 +57,7 @@ def test_upload_bad_file(ASSETS, ACCOUNTS, app):
         'audio[]': [(f, 'blank')]
     }
     response = app.post('/batch/audio/ingest', data=data)
-  
+
   assert response.status_code == 400
 
 def test_upload_mp3_faculty(ASSETS, ACCOUNTS, app):
@@ -67,7 +67,7 @@ def test_upload_mp3_faculty(ASSETS, ACCOUNTS, app):
         'audio[]': [(f, 'audio.mp3')]
     }
     response = app.post('/batch/audio/ingest', data=data)
-  
+
   assert response.status_code == 401, "Non-superuser faculty can upload file"
 
 def test_upload_mp3_restricted(ASSETS, ACCOUNTS, app):
@@ -79,7 +79,7 @@ def test_upload_mp3_restricted(ASSETS, ACCOUNTS, app):
         'audio[]': [(f, 'audio.mp3')]
     }
     response = app.post('/batch/audio/ingest', data=data)
-  
+
   assert response.status_code == 401, "Student can upload MP3"
 
 def test_upload_multiple_mp3s(ASSETS, ACCOUNTS, app):
@@ -101,3 +101,33 @@ def test_upload_wrong_form_name(ASSETS, ACCOUNTS, app):
     }
     response = app.post('/batch/audio/ingest', data=data)
     assert response.status_code == 400
+
+def test_retrieve_audio_with_bad_collection(ASSETS, ACCOUNTS, app):
+  app.login(ACCOUNTS['SUPERUSER'])
+
+  with open(ASSETS + 'stapler.mp3') as f:
+    data = {
+        'audio[]': [(f, 'audio.mp3')]
+    }
+    response = app.post('/batch/audio/ingest', data=data)
+
+  data = json.loads(response.data)
+  pid = data[0]['pid']
+
+  c = app.post('/collection', data=json.dumps({}), headers={'Content-Type': 'application/json'})
+  data = json.loads(c.data)
+  col_pid = data['pid']
+
+  # attach audio to collection
+  membership = [{"collection":{"id":col_pid,"title":"Something"},"videos":[pid]}]
+  membership_result = app.post('/batch/video/membership', data=json.dumps(membership), headers={'Content-Type': "application/json"})
+  assert membership_result.status_code is 200
+
+  data = json.loads(app.get('/video/' + pid))
+  assert(len(data['ma:isMemberOf']) is 1)
+
+  # delete the collection
+  app.delete('/collection'/ + col_pid)
+  result = app.get('/video/' + pid)
+  data = json.loads(result.data)
+  assert(len(data['ma:isMemberOf']) is 0)
